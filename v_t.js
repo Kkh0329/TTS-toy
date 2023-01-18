f3130c4675428727d3493fb083b90ee5fac44fd1
Kkh0329 <114138758+Kkh0329@users.noreply.github.com>
2023-01-18 14:29:29 +0900

    Create v_t.js

---
+++ v_t.js
@@ -0,0 +1,17 @@
+export const VOICE_TYPE = {
+    native: ["en-GB", "en-US", "en_US"],
+    esl: [
+      "ja-JP",
+      "ko-KR",
+      "ko_KR",
+      "pl-PL",
+      "de-DE",
+      "en-ES",
+      "fr-FR",
+      "hi-IN",
+      "ru-RU",
+      "zh-CN",
+      "zh-HK",
+      "zh-TW",
+    ],
+  };
